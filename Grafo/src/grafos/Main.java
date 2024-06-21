c79acfd3542edb8ca93bd74971004a75e78bf77a
Romeo Noro Guterres <romeo.noro@ufn.edu.br>
2024-06-21 13:20:05 -0300

    Update Main.java

--- Grafo/src/grafos/Main.java
+++ Grafo/src/grafos/Main.java
@@ -5,21 +5,20 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		//ler o arquivo e montar o grafo
+		//LER O ARQUIVO E MONTAR GRAFO
 		ArrayList<String> vertices = new ArrayList<>();
 		ArrayList<String> linhasDoArquivo = new ArrayList<>();
 		String nomeArquivo = "C:\\Users\\laboratorio\\eclipse-workspace\\Grafo\\src\\grafos\\mapaCentral.txt";
 		
 		Grafo.lerArquivo_montarGrafo(nomeArquivo, vertices, linhasDoArquivo);
 		Grafo g = new Grafo(vertices);
-		//montar as conexões do arquivo - SIMÉTRICO
+		//MONTAR AS CONEXÕES DO ARQUIVO - SIMÉTRICO 
 		g.montarConexoesSimetricas(linhasDoArquivo);
-		//exibir o grafo
+		//EXIBIR O GRAFO
 		g.mostrarMatriz();
-		//ler o arquivo e montar as conexões
-		g.montarConexoesSimetricas(linhasDoArquivo);
-		g.mostrarGrafo();	
-		//System.out.println("Tem caminho entre Agudo e Júlio de Castilhos? " + g.temCaminhoProfundidade);
-		g.temCaminhoProfundidade(nomeArquivo, nomeArquivo); //(origem:"AGUDO", destino:"JULIO DE CASTILHOS");
+		//g.mostrarGrafo();	
+		//LER O ARQUIVO E MONTAR AS CONEXÕES
+		//System.out.println("Tem caminho entre Agudo e Julio de Castilhos? " + g.temCaminhoProfundidade("AGUDO", "JULIO DE CASTILHOS"));
+		g.temCaminhoProfundidade("AGUDO", "JULIO DE CASTILHOS"); //g.temCaminhoProfundidade(nomeArquivo, nomeArquivo); 
 	}
 }
